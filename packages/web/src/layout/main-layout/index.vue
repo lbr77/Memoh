8b98e7fba882d875a6ad07946fe8f015d6d2109f
Acbox <acbox0328@gmail.com>
2026-03-01 02:40:07 +0800

    refactor(web): sidebar

--- packages/web/src/layout/main-layout/index.vue
+++ packages/web/src/layout/main-layout/index.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="flex">
-    <sidebar-provider class="**:data-[sidebar=sidebar]:bg-transparent">
+    <sidebar-provider :default-open="sidebarDefaultOpen" class="**:data-[sidebar=sidebar]:bg-transparent">
       <slot name="sidebar" />
       <slot name="main" />
     </sidebar-provider>
@@ -9,4 +9,5 @@
 <script setup lang="ts">
 import { SidebarProvider } from '@memoh/ui'
 
+const sidebarDefaultOpen = document.cookie.includes('sidebar_state=true')
 </script>
